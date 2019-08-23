d35bb9ed0257bcea3bd90cbc98a50ce136a978e4
Luis <luis.p.melgar@disney.com>
2019-08-22 17:16:41 -0700

    using ajax to login

--- src/main/java/edu/udacity/java/nano/chat/Message.java
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -4,5 +4,14 @@ package edu.udacity.java.nano.chat;
  * WebSocket message model
  */
 public class Message {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     // TODO: add message model.
+    private String userName;
 }
